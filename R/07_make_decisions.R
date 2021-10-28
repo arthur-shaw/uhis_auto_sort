2abbdf9b966df9016957b34197861c2cf2445628
Arthur Shaw <47256431+arthur-shaw@users.noreply.github.com>
2021-10-28 13:21:46 -0400

    Prototype decision-making script--to be adapted once decisions made on server upgrade/API access

--- R/07_make_decisions.R
+++ R/07_make_decisions.R
@@ -4,9 +4,9 @@
 
 objects_needed <- c(
     "comments",
-    "issues",
-    "cases_to_review",
-    "interview_stats"
+    "issues", # TODO: change to `issues_plus_miss_and_suso` once API user available
+    "cases_to_review"
+    # TODO: add back once API user available: , "interview_stats"
 )
 
 check_exists(objects_needed)
@@ -19,15 +19,14 @@ check_exists(objects_needed)
 # returns a data frame of cases that contain comments
 interviews_with_comments <- susoreview::check_for_comments(
     df_comments = comments, 
-    df_issues = issues, 
+    df_issues = issues, # TODO: change to `issues_plus_miss_and_suso` once API user available
     df_cases_to_review = cases_to_review
 )
-
 
 # decide what action to take 
 decisions <- susoreview::decide_action(
     df_cases_to_review = cases_to_review,
-    df_issues = issues_plus_miss_and_suso,
+    df_issues = issues, # TODO: change to `issues_plus_miss_and_suso` once API user available
     df_has_comments = interviews_with_comments,
     df_interview_stats = interview_stats
 )
@@ -37,7 +36,7 @@ to_reject <- decisions[["to_reject"]]
 
 to_reject <- susoreview::add_rejection_msgs(
     df_to_reject = to_reject,
-    df_issues = issues_plus_miss_and_suso
+    df_issues = issues # TODO: change to `issues_plus_miss_and_suso` once API user available
 )
 
 # flag persistent issues
@@ -45,3 +44,53 @@ revised_decisions <- susoreview::flag_persistent_issues(
     df_comments = comments,
     df_to_reject = to_reject
 )
+
+# =============================================================================
+# Extract decisions into data representing them
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# To reject
+# -----------------------------------------------------------------------------
+
+to_reject_ids <- revised_decisions[["to_reject"]] %>%
+    dplyr::select(interview__id) %>%
+    dplyr::left_join(cases_to_review, by = "interview__id")
+
+to_reject_issues <- to_reject_ids %>%
+    dplyr::left_join(issues_plus_miss_and_suso)
+
+to_reject_api <- revised_decisions[["to_reject"]]
+
+# -----------------------------------------------------------------------------
+# To review
+# -----------------------------------------------------------------------------
+
+to_review_ids <- decisions[["to_review"]]
+
+to_review_issues <- to_review_ids %>%
+    dplyr::left_join(
+        issues_plus_miss_and_suso, 
+        by = c("interview__id", "interview__key")
+    )
+
+# -----------------------------------------------------------------------------
+# To follow up
+# -----------------------------------------------------------------------------
+
+to_follow_up_ids <- revised_decisions[["to_follow_up"]] %>%
+    dplyr::left_join(cases_to_review, by = "interview__id") %>%
+    dplyr::select(interview__id, interview__key)
+
+to_follow_up_issues <- revised_decisions[["to_follow_up"]] %>%
+    dplyr::left_join(issues_plus_miss_and_suso, by = "interview__id") %>%
+    dplyr::left_join(
+        cases_to_review, 
+        by = c("interview__id", "interview__key")
+    ) %>%
+    dplyr::select(
+        interview__id, interview__key, interview__status,
+        dplyr::starts_with("issue_")
+    )
+
+to_follow_up_api <- revised_decisions[["to_follow_up"]]
